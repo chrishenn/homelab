0dbf597a7ac22f1218e44bf104a7985e4534c8ef
chrishenn <chenn@alum.mit.edu>
2026-02-27 03:34:32 -0600

    feat: seafile works

---
+++ seafile/settings.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+SECRET_KEY = "t)ygmim^t*-55=-ugj77)ji(0zc%e_)e8^w_82f(^brkn49s@a"
+
+TIME_ZONE = "America/Chicago"
+
+OFFICE_SERVER_TYPE = "CollaboraOffice"
+ENABLE_OFFICE_WEB_APP = True
+OFFICE_WEB_APP_BASE_URL = "https://collabora.chenn.dev/hosting/discovery"
+WOPI_ACCESS_TOKEN_EXPIRATION = 30 * 60
+OFFICE_WEB_APP_FILE_EXTENSION = (
+    "odp",
+    "ods",
+    "odt",
+    "xls",
+    "xlsb",
+    "xlsm",
+    "xlsx",
+    "ppsx",
+    "ppt",
+    "pptm",
+    "pptx",
+    "doc",
+    "docm",
+    "docx",
+)
+ENABLE_OFFICE_WEB_APP_EDIT = True
+OFFICE_WEB_APP_EDIT_FILE_EXTENSION = (
+    "odp",
+    "ods",
+    "odt",
+    "xls",
+    "xlsb",
+    "xlsm",
+    "xlsx",
+    "ppsx",
+    "ppt",
+    "pptm",
+    "pptx",
+    "doc",
+    "docm",
+    "docx",
+)
